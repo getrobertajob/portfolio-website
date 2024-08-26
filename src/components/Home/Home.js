8dc4d44a35155012837504a0be9adac8a5bb23dc
Robert Lute <getrobertajob@gmail.com>
2024-08-26 16:35:17 -0600

    swap home img 2

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/home-main.svg";
+import homeCode from "../../Assets/home-code.gif";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
@@ -33,7 +34,7 @@ function Home() {
  
             <Col md={5}>
               <img
-                src={homeLogo}
+                src={homeCode}
                 alt="home pic"
                 className="img-fluid"
                 style={{ maxHeight: "450px" }}
